15bc85bcb7e611517c2435686ec99e397874c2be
Emiel de Jong <emieldejong@xs4all.nl>
2020-12-17 15:39:09 +0100

    first commit

---
+++ src/main/java/nl/saxofoonleren/demo_spring_jpa/model/Klant.java
@@ -0,0 +1,25 @@
+package nl.saxofoonleren.demo_spring_jpa.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Klant {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "klant_nr)
+    private String klantNr;
+
+}
